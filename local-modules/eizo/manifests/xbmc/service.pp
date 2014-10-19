140ebaea683c470fbc61dad4308f2c02fd580fbd
Vincent Bernat <vincent@bernat.im>
2014-10-19 08:47:30 +0200

    xbmc: use a wrapper to start xbmc
    We need a terminal to be allowed to start X. This seems better than
    allowing everyone to start X.

--- local-modules/eizo/manifests/xbmc/service.pp
+++ local-modules/eizo/manifests/xbmc/service.pp
@@ -1,5 +1,11 @@
 class eizo::xbmc::service inherits eizo::xbmc {
 
+  file { '/usr/local/bin/xbmc-xinit':
+    ensure => present,
+    source => "puppet:///modules/eizo/xbmc/xbmc-xinit",
+    mode => 'a+x'
+  }
+  ->
   file { '/etc/systemd/system/xbmc.service':
     ensure => present,
     source => "puppet:///modules/eizo/xbmc/xbmc.service"
