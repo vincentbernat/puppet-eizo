918f663f60524cbfec666171cb4c1116daa0531b
Vincent Bernat <vincent@bernat.im>
2014-10-19 20:43:16 +0200

    transmission: fix transmission startup by providing appropriate arguments
    The configuration file was not provided correctly to the daemon through
    systemd.

--- local-modules/eizo/manifests/transmission/service.pp
+++ local-modules/eizo/manifests/transmission/service.pp
@@ -1,5 +1,16 @@
 class eizo::transmission::service inherits eizo::transmission {
 
+  file { '/etc/systemd/system/transmission-daemon.service':
+    ensure => present,
+    source => "puppet:///modules/eizo/transmission/transmission-daemon.service"
+  }
+  ~>
+  exec { 'reload systemd for transmission':
+    path => [ '/bin', '/sbin' ],
+    refreshonly => true,
+    command => 'systemctl daemon-reload'
+  }
+  ->
   service { 'transmission-daemon':
     ensure => running,
     enable => true
