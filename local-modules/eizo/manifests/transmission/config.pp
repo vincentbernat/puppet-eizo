5d6617a36c1ab92db81d84d015bc1e3b008e89bf
Vincent Bernat <vincent@bernat.im>
2014-10-19 21:16:59 +0200

    transmission: config file is overwritten on exit
    Reload should take care of this problem. At some point, there is
    convergence...

--- local-modules/eizo/manifests/transmission/config.pp
+++ local-modules/eizo/manifests/transmission/config.pp
@@ -3,9 +3,9 @@ class eizo::transmission::config inherits eizo::transmission {
   file { '/etc/transmission-daemon/settings.json':
     ensure => present,
     content => template("eizo/transmission/settings.json.erb"),
-    owner => "root",
-    group => "debian-transmission",
-    mode => "0640"
+    mode => "0600",
+    owner => "debian-transmission",
+    group => "debian-transmission"
   }
 
 }
