9b3589622e9def0f591e0ac64d470c6d4f1dba03
Vincent Bernat <vincent@bernat.im>
2014-10-21 01:22:21 +0200

    transmission: don't erase transmission config file all the time
    transmission-deamon modifies this file at each run. Use another file for
    configuration and copy it over only if different from the last run.

--- local-modules/eizo/manifests/transmission/config.pp
+++ local-modules/eizo/manifests/transmission/config.pp
@@ -1,8 +1,18 @@
 class eizo::transmission::config inherits eizo::transmission {
 
-  file { '/etc/transmission-daemon/settings.json':
+  file { '/etc/transmission-daemon/settings.json.puppet':
     ensure => present,
     content => template("eizo/transmission/settings.json.erb"),
+  }
+  ~>
+  exec { "copy transmission-daemon settings":
+    refreshonly => true,
+    path => [ '/bin', '/sbin' ],
+    refreshonly => true,
+    command => 'cp /etc/transmission-daemon/settings.json.puppet /etc/transmission-daemon/settings.json'
+  }
+  ~>
+  file { "/etc/transmission-daemon/settings.json":
     mode => "0600",
     owner => "debian-transmission",
     group => "debian-transmission"
