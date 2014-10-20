8d2cdaf35c4b5c5a181f034059303dd903aea88c
Vincent Bernat <vincent@bernat.im>
2014-10-20 19:58:08 +0200

    xbmc: don't force EDID
    While this seems a good idea, it must be coupled with the ability of X
    to force an output. Moreover, this doesn't work for audio, as explained
    in this bug report:
     https://bugs.freedesktop.org/show_bug.cgi?id=82349
    
    We'll try something else instead.

--- local-modules/eizo/manifests/xbmc/config.pp
+++ local-modules/eizo/manifests/xbmc/config.pp
@@ -1,20 +1,5 @@
 class eizo::xbmc::config inherits eizo::xbmc {
 
-  # Setup a custom EDID if one is provided
-  if ($edid) {
-    $blob = base64('decode', $edid['edid'])
-    $output = $edid['output']
-
-    file { '/lib/firmware/edid': ensure => directory }
-    file { "/lib/firmware/edid/edid-${output}.bin":
-      ensure => present,
-      content => $blob
-    }
-    file { '/usr/local/bin/xbmc-edid':
-      ensure => present,
-      mode => 'a+x',
-      source => "puppet:///modules/eizo/xbmc/xbmc-edid"
-    }
-  }
+  # Nothing to do
 
 }
