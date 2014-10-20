748df25a394e20e09130477066bb34a31aaa865c
Vincent Bernat <vincent@bernat.im>
2014-10-21 01:06:32 +0200

    flexget: run cron command

--- local-modules/eizo/manifests/flexget/service.pp
+++ local-modules/eizo/manifests/flexget/service.pp
@@ -1,7 +1,7 @@
 class eizo::flexget::service inherits eizo::flexget {
 
   cron { flexget:
-    command => "${home}/venv/bin/flexget",
+    command => "${home}/venv/bin/flexget --cron",
     user => "flexget",
     minute => '*/15'
   }
