594b565a134c5efce3a9a8a5ef6b97f56ea95cc6
Vincent Bernat <vincent@bernat.im>
2014-10-20 22:08:29 +0200

    xbmc: only start when screen is connected
    And let systemd tries that every 15 seconds. Not high tech but should do
    the trick.

--- local-modules/eizo/manifests/xbmc/service.pp
+++ local-modules/eizo/manifests/xbmc/service.pp
@@ -1,15 +1,5 @@
 class eizo::xbmc::service inherits eizo::xbmc {
 
-  udev::rule { "99-xbmc.rules":
-    source => "puppet:///modules/eizo/xbmc/xbmc.rules"
-  }
-  ->
-  file { '/etc/systemd/system/xbmc.target':
-    ensure => present,
-    source => "puppet:///modules/eizo/xbmc/xbmc.target",
-    notify => Exec['reload systemd for xbmc']
-  }
-  ->
   file { '/etc/systemd/system/xbmc.service':
     ensure => present,
     source => "puppet:///modules/eizo/xbmc/xbmc.service"
@@ -22,7 +12,8 @@ class eizo::xbmc::service inherits eizo::xbmc {
   }
   ->
   service { 'xbmc':
-    enable => true
+    enable => true,
+    ensure => running
   }
 
 }
