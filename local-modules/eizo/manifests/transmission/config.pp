e903c37ac20ed260622673ed4c9afe2b474328a9
Vincent Bernat <vincent@bernat.im>
2014-10-22 07:25:54 +0200

    transmssion: fix permissions of temporary config file

--- local-modules/eizo/manifests/transmission/config.pp
+++ local-modules/eizo/manifests/transmission/config.pp
@@ -3,6 +3,7 @@ class eizo::transmission::config inherits eizo::transmission {
   file { '/etc/transmission-daemon/settings.json.puppet':
     ensure => present,
     content => template("eizo/transmission/settings.json.erb"),
+    mode => "0600"
   }
   ~>
   exec { "copy transmission-daemon settings":
