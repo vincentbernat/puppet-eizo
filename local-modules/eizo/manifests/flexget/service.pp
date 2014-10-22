e8f8779260a63f44fafb20726b28d58bd93572c8
Vincent Bernat <vincent@bernat.im>
2014-10-22 15:42:55 +0200

    flexget: run once per hour and simplify tasks
    Instead of having a lot of tasks, just have one for series, one for
    anime and use the inputs plugin to specify several inputs. The order of
    the inputs give the priority.

--- local-modules/eizo/manifests/flexget/service.pp
+++ local-modules/eizo/manifests/flexget/service.pp
@@ -3,7 +3,7 @@ class eizo::flexget::service inherits eizo::flexget {
   cron { flexget:
     command => "${home}/venv/bin/flexget execute --cron",
     user => "flexget",
-    minute => '*/15'
+    minute => '18'
   }
 
 }
