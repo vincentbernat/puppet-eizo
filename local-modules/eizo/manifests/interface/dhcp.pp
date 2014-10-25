8c3bd21895d2bb350f9eb3b158588901573283a3
Vincent Bernat <vincent@bernat.im>
2014-10-25 17:09:11 +0200

    interfaces: fix DHCP invocation

--- local-modules/eizo/manifests/interface/dhcp.pp
+++ local-modules/eizo/manifests/interface/dhcp.pp
@@ -1,7 +1,7 @@
 define eizo::interface::dhcp {
 
   exec { "enable DHCP for $name":
-    cmd => "systemctl enable dhclient@$name",
+    command => "systemctl enable dhclient@$name",
     path => [ "/bin", "/sbin" ],
     creates => "/etc/systemd/system/multi-user.target.wants/dhclient@$name.service"
   }
