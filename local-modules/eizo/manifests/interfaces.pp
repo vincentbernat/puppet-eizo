b1ff6de67e9b79b7e25988e7bb3c8d8e27c8770c
Vincent Bernat <vincent@bernat.im>
2014-10-25 16:21:39 +0200

    interfaces: enable interface naming through systemd/networkd/udev

--- local-modules/eizo/manifests/interfaces.pp
+++ local-modules/eizo/manifests/interfaces.pp
@@ -1,11 +1,28 @@
 # Configure physical interfaces. No IP configuration is done here. We
 # just create the physical and logical (VLAN) interfaces. The
 # configuration is done through systemd.
+#
+# See this bug:
+#  https://bugs.freedesktop.org/show_bug.cgi?id=85440
+#
 class eizo::interfaces {
 
   service { "systemd-networkd":
     ensure => running,
     enable => true
+  }
+
+  shellvar { 'GRUB_CMDLINE_LINUX':
+    ensure       => present,
+    target       => '/etc/default/grub',
+    value        => 'net.ifnames=1',
+    array_append => true,
+    array_type   => 'double',
+  }
+  ~>
+  exec { "udpate GRUB configuration":
+    cmd => "/usr/sbin/update-grub",
+    refreshonly => true
   }
 
   create_resources(
