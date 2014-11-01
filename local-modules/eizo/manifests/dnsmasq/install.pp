238d3e638e5632d14b0a70318de076bc64b85201
Vincent Bernat <vincent@bernat.im>
2014-11-01 10:37:38 +0100

    dnsmasq: use a persistent lease file

--- local-modules/eizo/manifests/dnsmasq/install.pp
+++ local-modules/eizo/manifests/dnsmasq/install.pp
@@ -3,5 +3,10 @@ class eizo::dnsmasq::install inherits eizo::dnsmasq {
   package { 'dnsmasq':
     ensure => present
   }
+  ->
+  file { '/var/lib/dnsmasq':
+    ensure => "directory",
+    owner => "dnsmasq"
+  }
 
 }
