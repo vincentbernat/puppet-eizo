430995bb7a62e299571775ce4d8c66a018aff5f7
Vincent Bernat <vincent@bernat.im>
2014-11-24 23:56:02 +0100

    flexget: run twice per hour

--- local-modules/eizo/manifests/flexget/service.pp
+++ local-modules/eizo/manifests/flexget/service.pp
@@ -3,7 +3,7 @@ class eizo::flexget::service inherits eizo::flexget {
   cron { flexget:
     command => "${home}/venv/bin/flexget execute --cron",
     user => "flexget",
-    minute => '18'
+    minute => '*/22'
   }
 
 }
