578cb8f86d4b64814043e120ee702a989c2db01a
Vincent Bernat <vincent@bernat.im>
2014-11-28 16:46:37 +0100

    ssh: factor a bit the configuration

--- local-modules/eizo/manifests/ssh.pp
+++ local-modules/eizo/manifests/ssh.pp
@@ -1,32 +1,30 @@
 class eizo::ssh {
 
-  package { "openssh-server": ensure => installed }
+  package { 'openssh-server': ensure => installed }
 
-  sshd_config { "PermitRootLogin":
+  sshd_config { 'PermitRootLogin':
     ensure => present,
-    value => "no",
-    require => Package["openssh-server"]
+    value  => 'no'
   }
 
-  sshd_config { "PasswordAuthentication":
+  sshd_config { 'PasswordAuthentication':
     ensure => present,
-    value => "no",
-    require => Package["openssh-server"]
+    value  => 'no'
   }
 
-  sshd_config { "UseDNS":
+  sshd_config { 'UseDNS':
     ensure => present,
-    value => "no",
-    require => Package["openssh-server"]
+    value  => 'no'
   }
 
   Sshd_config {
-    notify => Service[ssh]
+    notify  => Service[ssh],
+    require => Package['openssh-server']
   }
 
-  service { "ssh":
-    ensure => running,
-    require => Package["openssh-server"]
+  service { 'ssh':
+    ensure  => running,
+    require => Package['openssh-server']
   }
 
 }
