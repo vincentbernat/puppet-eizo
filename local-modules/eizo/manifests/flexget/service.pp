f2b6084a997e97f4924a9c8bc7889c801a5a7748
Vincent Bernat <vincent@bernat.im>
2014-12-28 21:36:00 +0100

    flexget: move `--cron` argument before `execute` command

--- local-modules/eizo/manifests/flexget/service.pp
+++ local-modules/eizo/manifests/flexget/service.pp
@@ -1,7 +1,7 @@
 class eizo::flexget::service inherits eizo::flexget {
 
   cron { flexget:
-    command => "${home}/venv/bin/flexget execute --cron",
+    command => "${home}/venv/bin/flexget --cron execute",
     user => "flexget",
     minute => '*/22'
   }
