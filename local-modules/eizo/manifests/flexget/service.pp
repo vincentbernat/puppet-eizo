8f5c3c7e51d3e4604d8f00ebfa238479bb6ddd3f
Vincent Bernat <vincent@bernat.im>
2014-12-28 21:49:39 +0100

    flexget: filter out HTTPS warning from cron output

--- local-modules/eizo/manifests/flexget/service.pp
+++ local-modules/eizo/manifests/flexget/service.pp
@@ -1,7 +1,7 @@
 class eizo::flexget::service inherits eizo::flexget {
 
   cron { flexget:
-    command => "${home}/venv/bin/flexget --cron execute",
+    command => "(${home}/venv/bin/flexget --cron execute 3>&1 1>&2 2>&3 | grep -v InsecureRequestWarning) 3>&1 1>&2 2>&3",
     user => "flexget",
     minute => '*/22'
   }
