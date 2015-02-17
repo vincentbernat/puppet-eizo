5f178d7c0eef57655c9f33e27d571448a9feda7b
Vincent Bernat <vincent@bernat.im>
2015-02-17 22:18:55 +0100

    tor: move site-specific configuration to hiera
    DNS port is usually 53. Our value is site-specific because of the
    redirect in the firewall.

--- local-modules/eizo/manifests/tor.pp
+++ local-modules/eizo/manifests/tor.pp
@@ -1,4 +1,4 @@
-class eizo::tor($bind, $transport = 9040, $dnsport = 9041, $bandwidth = "1 MBytes") {
+class eizo::tor($bind, $transport, $dnsport = 53, $bandwidth = "1 MBytes") {
 
   class { '::eizo::tor::install': } ->
   class { '::eizo::tor::config':  } ~>
