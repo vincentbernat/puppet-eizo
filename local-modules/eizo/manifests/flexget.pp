fe09829cbc0dce849af42c62516b630d70a0cf1a
Vincent Bernat <vincent@bernat.im>
2015-04-30 10:06:08 +0200

    flexget: upgrade to 1.2.303

--- local-modules/eizo/manifests/flexget.pp
+++ local-modules/eizo/manifests/flexget.pp
@@ -1,4 +1,4 @@
-class eizo::flexget($home, $config, $version="1.2.277") {
+class eizo::flexget($home, $config, $version="1.2.303") {
 
   class { '::eizo::flexget::install': } ->
   class { '::eizo::flexget::config':  } ~>
