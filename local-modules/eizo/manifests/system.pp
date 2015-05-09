82f7257c1844b36bacadd2cf10d0f5dd0cd3f63c
Vincent Bernat <vincent@bernat.im>
2015-05-09 21:42:30 +0200

    system: only log with systemd-journal

--- local-modules/eizo/manifests/system.pp
+++ local-modules/eizo/manifests/system.pp
@@ -4,4 +4,19 @@ class eizo::system {
     source => "puppet:///modules/eizo/system/logind.conf"
   }
 
+  # Log with systemd-journald only
+  package { ["rsyslog", "syslog-ng"]:
+    ensure => absent
+  }
+  file { "/var/log/journal":
+    ensure => directory,
+    group => "systemd-journal"
+  }
+  exec { "Set ACL on /var/log/journal":
+    command => "/usr/bin/setfacl -R -nm g:adm:rx,d:g:adm:rx /var/log/journal",
+    unless => "/usr/bin/getfacl /var/log/journal | /bin/grep '^group:adm:r-x' && /usr/bin/getfacl /var/log/journal | /bin/grep '^default:group:adm:r-x'",
+    require => Package["acl"]
+  }
+  package { "acl": ensure => installed }
+
 }
