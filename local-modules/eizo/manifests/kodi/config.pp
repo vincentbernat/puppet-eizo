1bb28b157953494a0d7a8f58ed7e3b0cf2c965db
Vincent Bernat <vincent@bernat.im>
2015-06-05 22:22:26 +0200

    kodi: crontab for Artwork Downloader script

--- local-modules/eizo/manifests/kodi/config.pp
+++ local-modules/eizo/manifests/kodi/config.pp
@@ -1,5 +1,12 @@
 class eizo::kodi::config inherits eizo::kodi {
 
-  # Nothing to do
+  # Setup a crontab for Artwork Downloader
+  cron { kodi-artwork-downloader:
+    command => '/usr/bin/kodi-send --action="RunScript(script.artwork.downloader,silent=true)"',
+    user => 'xbmc',
+    hour => 3,
+    minute => 4,
+    day => '*/3'
+  }
 
 }
