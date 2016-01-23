8b7e37b0e58153ae18354d094c830c0e9e6a6dc5
Vincent Bernat <vincent@bernat.im>
2016-01-23 11:17:53 +0100

    kodi: schedule a daily video cleanup

--- local-modules/eizo/manifests/kodi/config.pp
+++ local-modules/eizo/manifests/kodi/config.pp
@@ -9,6 +9,14 @@ class eizo::kodi::config inherits eizo::kodi {
     monthday => '*/3'
   }
 
+  # And regular cleanup of video library
+  cron { kodi-clean-library:
+    command => '/usr/bin/kodi-send --action="CleanLibrary(video,silent=true)"',
+    user => 'xbmc',
+    hour => 3,
+    minute => 45
+  }
+
   # Install our own advanced settings
   file { "${home}/userdata/advancedsettings.xml":
     source => "puppet:///modules/eizo/kodi/advancedsettings.xml"
