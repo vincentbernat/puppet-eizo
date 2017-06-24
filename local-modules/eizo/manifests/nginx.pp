4e32ebd2a7d889e06fc8702b9af594ddbc9ff34b
Vincent Bernat <vincent@bernat.im>
2017-06-24 08:37:40 +0200

    nginx: move more resources outside of classes

--- local-modules/eizo/manifests/nginx.pp
+++ local-modules/eizo/manifests/nginx.pp
@@ -14,24 +14,26 @@ class eizo::nginx {
     { notify => Service[nginx] })
 
   create_resources(
-    crl,
+    eizo::nginx::crl,
     hiera_hash("eizo::nginx::crls", {}),
     { notify => Service[nginx] })
 
-  define crl($path=$title, $url) {
-    cron { "update-crl-${path}":
-      minute => 3,
-      hour => 4,
-      user => root,
-      command => "t=$(mktemp -p $(dirname ${path})) && curl -s -o \$t ${url} && chmod 644 \$t && mv \$t ${path} && /usr/sbin/nginx -s reload"
-    }
-    exec { "retrieve-${path}":
-      path => ["/bin", "/usr/bin"],
-      command => "sh -c 't=$(mktemp -p $(dirname ${path})) && curl -s -o \$t ${url} && chmod 644 \$t && mv \$t ${path}'",
-      creates => "${path}"
-    }
+}
+
+define eizo::nginx::crl($path=$title, $url) {
+  cron { "update-crl-${path}":
+    minute => 3,
+    hour => 4,
+    user => root,
+    command => "t=$(mktemp -p $(dirname ${path})) && curl -s -o \$t ${url} && chmod 644 \$t && mv \$t ${path} && /usr/sbin/nginx -s reload"
+  }
+  exec { "retrieve-${path}":
+    path => ["/bin", "/usr/bin"],
+    command => "sh -c 't=$(mktemp -p $(dirname ${path})) && curl -s -o \$t ${url} && chmod 644 \$t && mv \$t ${path}'",
+    creates => "${path}"
   }
 }
+
 
 class eizo::nginx::acme {
 
