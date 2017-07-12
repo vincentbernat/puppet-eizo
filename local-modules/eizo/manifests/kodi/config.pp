1b989d408ab88a75b4477cdf7808be409a18691e
Vincent Bernat <vincent@bernat.im>
2017-07-12 08:12:14 +0200

    kodi: silent kodi-send

--- local-modules/eizo/manifests/kodi/config.pp
+++ local-modules/eizo/manifests/kodi/config.pp
@@ -2,7 +2,7 @@ class eizo::kodi::config inherits eizo::kodi {
 
   # Setup a crontab for Artwork Downloader
   cron { kodi-artwork-downloader:
-    command => '/usr/bin/kodi-send --action="RunScript(script.artwork.downloader,silent=true)"',
+    command => '/usr/bin/kodi-send --action="RunScript(script.artwork.downloader,silent=true)" > /dev/null',
     user => 'xbmc',
     hour => 3,
     minute => 4,
@@ -11,7 +11,7 @@ class eizo::kodi::config inherits eizo::kodi {
 
   # And regular cleanup of video library
   cron { kodi-clean-library:
-    command => '/usr/bin/kodi-send --action="CleanLibrary(video,silent=true)"',
+    command => '/usr/bin/kodi-send --action="CleanLibrary(video,silent=true)" > /dev/null',
     user => 'xbmc',
     hour => 3,
     minute => 45
