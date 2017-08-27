68d7a258edf0c06a2974d0340b8836c20aec69ef
Vincent Bernat <vincent@bernat.im>
2017-08-27 17:40:07 +0200

    flexget: upgrade to 2.10.82

--- local-modules/eizo/manifests/flexget.pp
+++ local-modules/eizo/manifests/flexget.pp
@@ -3,7 +3,7 @@ class eizo::flexget(
   $config,
   $move_from,
   $move_to,
-  $version="1.2.521") {
+  $version="2.10.82") {
 
   class { '::eizo::flexget::install': } ->
   class { '::eizo::flexget::config':  } ~>
