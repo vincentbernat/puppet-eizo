8fa6b7ead86d09919485116949c1b82c664355fb
Vincent Bernat <vincent@bernat.im>
2018-02-13 16:58:02 +0100

    flexget: only run twice an hour

--- local-modules/eizo/manifests/flexget/service.pp
+++ local-modules/eizo/manifests/flexget/service.pp
@@ -3,7 +3,7 @@ class eizo::flexget::service inherits eizo::flexget {
   cron { flexget:
     command => "/usr/local/bin/flexget-cron run",
     user    => "flexget",
-    minute  => '*/22'
+    minute  => '*/33'
   }
 
   cron { flexget-move:
