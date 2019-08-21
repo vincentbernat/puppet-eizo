116197fa94de2df4bb74a52c53bf4d0f1a851121
Vincent Bernat <vincent@bernat.ch>
2019-08-21 19:59:33 +0200

    router: adapt configuration for Orange

--- local-modules/eizo/manifests/firewall/service.pp
+++ local-modules/eizo/manifests/firewall/service.pp
@@ -20,7 +20,7 @@ class eizo::firewall::service inherits eizo::firewall {
     source => "puppet:///modules/eizo/firewall/dhcp-firewall-hook"
   }
   file { '/etc/dhcp/dhclient-exit-hooks.d/000-ipv6':
-    source => "puppet:///modules/eizo/firewall/dhcp-ipv6-hook"
+    ensure => absent
   }
 
   exec { "firewall reload":
