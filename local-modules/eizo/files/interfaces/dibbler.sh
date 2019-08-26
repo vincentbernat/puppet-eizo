d3e505120f6c2c56c7e6cade0211c44ed6f5a7ef
Vincent Bernat <vincent@bernat.ch>
2019-08-26 15:47:36 +0200

    interfaces: log dibbler errors

--- local-modules/eizo/files/interfaces/dibbler.sh
+++ local-modules/eizo/files/interfaces/dibbler.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-set -x
-
+{
 case "$1" in
     delete)
-        ip -6 addr delete dev $IFNAME scope global
+        ip -6 addr delete dev $IFACE scope global
         ;;
     add|update)
-        ip -6 addr add ${PREFIX1%/*}1/128 dev $IFNAME scope global
+        ip -6 addr add ${PREFIX1}1/128 dev $IFACE scope global
         ;;
 esac
+} | logger --priority user.notice --tag dibbler 2>&1 | logger --priority user.error --tag dibbler
