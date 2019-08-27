a61e68d5fb153260da422c93326df4e6970faf0a
Vincent Bernat <vincent@bernat.ch>
2019-08-27 07:25:04 +0200

    interfaces: extract interface names from hiera for post-down

--- local-modules/eizo/manifests/interface/ifup.pp
+++ local-modules/eizo/manifests/interface/ifup.pp
@@ -9,6 +9,7 @@ define eizo::interface::ifup(
   $v6 = false,                  # get an IPv6 prefix from PD
   $also = {}) {
 
+  $interfaces = hiera_hash('eizo::interfaces')
   file { "/etc/network/interfaces.d/${order}-${name}":
     content => template("eizo/interfaces/ifup.erb")
   }
