cd8f962ea908d9f71042f9b2142071047911390b
Vincent Bernat <vincent@bernat.ch>
2022-04-17 18:37:04 +0200

    kodi: do a mysql backup as well

--- local-modules/eizo/manifests/kodi/config.pp
+++ local-modules/eizo/manifests/kodi/config.pp
@@ -1,20 +1,5 @@
 class eizo::kodi::config inherits eizo::kodi {
-
-  # And regular cleanup of video library
-  cron { kodi-clean-library:
-    command => '/usr/bin/kodi-send --action="CleanLibrary(video,silent=true)" > /dev/null',
-    user => 'xbmc',
-    hour => 3,
-    minute => 45
-  }
-  cron { kodi-update-library:
-    command => '/usr/bin/kodi-send --action="UpdateLibrary(video)" > /dev/null',
-    user => 'xbmc',
-    hour => "*/6",
-    minute => 5
-  }
-
-  # And keymaps
+  # Keymaps
   file { "${home}/userdata/keymaps/flirc.xml":
     source => "puppet:///modules/eizo/kodi/flirc.xml"
   }
--- local-modules/eizo/manifests/kodi/service.pp
+++ local-modules/eizo/manifests/kodi/service.pp
@@ -11,4 +11,24 @@ class eizo::kodi::service inherits eizo::kodi {
     ensure => running
   }
 
+  # And regular cleanup of video library
+  cron { kodi-backup:
+    command => 'mysqldump --all-databases | gzip -c > /srv/media/data/mysqldump-$(date -I).gz',
+    hour => 1,
+    minute => 10,
+    weekday => 1
+  }
+  cron { kodi-clean-library:
+    command => '/usr/bin/kodi-send --action="CleanLibrary(video,silent=true)" > /dev/null',
+    user => 'xbmc',
+    hour => 3,
+    minute => 45
+  }
+  cron { kodi-update-library:
+    command => '/usr/bin/kodi-send --action="UpdateLibrary(video)" > /dev/null',
+    user => 'xbmc',
+    hour => "*/6",
+    minute => 5
+  }
+
 }
