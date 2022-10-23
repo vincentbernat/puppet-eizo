a1b8296a636ed937dc4262235cd2a5b316cb964d
Vincent Bernat <vincent@bernat.ch>
2022-10-23 11:00:29 +0200

    nginx: use other locations for proxy/client_body temp path

--- local-modules/eizo/manifests/nginx.pp
+++ local-modules/eizo/manifests/nginx.pp
@@ -18,14 +18,6 @@ class eizo::nginx {
     lookup("eizo::nginx::crls", {merge=>hash}),
     { notify => Service[nginx] })
 
-  systemd::dropin_file { 'runtimedirectory.conf':
-    unit    => 'nginx.service',
-    content => @(EOT),
-      # Managed by Puppet
-      [Service]
-      RuntimeDirectory=nginx
-      | EOT
-  }
 }
 
 define eizo::nginx::crl($path=$title, $url) {
