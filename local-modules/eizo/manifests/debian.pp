6b11b924ca0d2b90da56d721cbc9c04a13bd4d81
Vincent Bernat <vincent@bernat.ch>
2024-06-09 10:37:17 +0200

    debian: enable non-free-firmware

--- local-modules/eizo/manifests/debian.pp
+++ local-modules/eizo/manifests/debian.pp
@@ -30,17 +30,17 @@ class eizo::debian(
   apt::source { "bookworm":
     location          => 'http://ftp.fr.debian.org/debian',
     release           => 'bookworm',
-    repos             => 'main contrib non-free',
+    repos             => 'main contrib non-free-firmware',
   }
   apt::source { "bookworm-security":
     location => 'http://security.debian.org/debian-security',
     release  => 'bookworm-security',
-    repos    => 'main contrib non-free',
+    repos    => 'main contrib non-free-firmware',
   }
   apt::source { "bookworm-backports":
     location          => 'http://ftp.fr.debian.org/debian',
     release           => 'bookworm-backports',
-    repos             => 'main contrib non-free',
+    repos             => 'main contrib',
   }
 
 }
